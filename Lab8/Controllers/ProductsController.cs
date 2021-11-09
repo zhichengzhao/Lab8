1d40aae419c9a3fe78d16aa21f4e7c25d2c4dbf9
zhichengzhao <60787822+zhichengzhao@users.noreply.github.com>
2021-11-09 17:22:17 -0500

    change the value of products

--- Lab8/Controllers/ProductsController.cs
+++ Lab8/Controllers/ProductsController.cs
@@ -12,9 +12,9 @@ namespace Lab8.Controllers
     {
 		Product[] products = new Product[]
 		{
-			new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-			new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-			new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+			new Product { Id = 1, Name = "Tomato Soup", Category = "Grocer", Price = 10 },
+			new Product { Id = 2, Name = "Yoo", Category = "Toys", Price = 3.89M },
+			new Product { Id = 3, Name = "Ham", Category = "Hardware", Price = 20.01M }
 		};
 
 
